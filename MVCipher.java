81c2235b976dba26e66f32d406ce0e72753af91a
neyneyb27 <neha.bayyapu@gmail.com>
2024-09-23 14:55:01 -0700

    Add files via upload

---
+++ MVCipher.java
@@ -0,0 +1,52 @@
+// imports go here
+
+/**
+ *	MVCipher - Add your description here
+ *	Requires Prompt and FileUtils classes.
+ *	
+ *	@author	
+ *	@since	
+ */
+public class MVCipher {
+	private int keyWord;
+	private int;
+	// fields go here
+		
+	/** Constructor */
+	public MVCipher() { }
+	
+	public static void main(String[] args) {
+		MVCipher mvc = new MVCipher();
+		mvc.run();
+	}
+	
+	/**
+	 *	Method header goes here
+	 */
+	public void run() {
+		System.out.println("\n Welcome to the MV Cipher machine!\n");
+		
+		/* Prompt for a key and change to uppercase
+		   Do not let the key contain anything but alpha
+		   Use the Prompt class to get user input */
+		System.out.println("Please input a word to use as key(letters only");
+		keyWord = prompt.getString();
+		
+		/* Prompt for encrypt or decrypt */
+			
+			
+		/* Prompt for an input file name */
+		
+		
+		/* Prompt for an output file name */
+		
+		
+		/* Read input file, encrypt or decrypt, and print to output file */
+		
+		
+		/* Don't forget to close your output file */
+	}
+	
+	// other methods go here
+	
+}
